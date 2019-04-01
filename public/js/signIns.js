52babc1cf68de95a921fcea754b6604e41b963db
zoe-gonzales <zoegonzales03@gmail.com>
2019-03-31 21:09:31 -0600

    initial functionality to mark attendees as present or absent

--- public/js/signIns.js
+++ public/js/signIns.js
@@ -16,4 +16,32 @@ $(document).ready(function(){
         });
 
     });
+
+    $(document).on('click', '.update', function(){
+        var itemToUpdate = $(this).data('type');
+
+        var id = $(this).data('id');
+
+        var updatedAttendee = {};
+
+        switch (itemToUpdate){
+            case 'editName':
+                // updatedAttendee.name = $(this)
+            break;
+            case 'present':
+                updatedAttendee.signedIn = 1;
+            break;
+            case 'absent':
+                updatedAttendee.onList = 0;
+            break;
+        }
+
+        $.ajax('/api/signins/' + id, {
+            method: 'PUT',
+            data: updatedAttendee
+        }).then(function(){
+            location.reload();
+        });
+        
+    });
 });
