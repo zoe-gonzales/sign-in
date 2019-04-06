9dbfc7d45edb9ed9a939123d9b893ddabaefbe86
zoe-gonzales <zoegonzales03@gmail.com>
2019-04-05 19:04:18 -0600

    setting environment variables for development

--- models/index.js
+++ models/index.js
@@ -7,12 +7,16 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+const development = require('../config/development');
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else if (env === "development") {
+  sequelize = new Sequelize(development.database, development.username, development.password, development);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
+
 }
 
 fs
